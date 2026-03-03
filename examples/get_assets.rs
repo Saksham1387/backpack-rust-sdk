0d4c57ccbbd7fbdb9112d128e836c46d320acc31
Saksham <saksham1387@gmail.com>
2026-03-03 20:01:17 +0530

    Removed the token

--- examples/get_assets.rs
+++ examples/get_assets.rs
@@ -3,7 +3,7 @@ use backpack_sdk_rust::BackpackClient;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create the client. No API key needed for public endpoints.
-    let client = BackpackClient::with_signer("edlNY3TrsB/awIIBYFhPjRnwnGFHoOEiyvNAxTMnpko=")?;
+    let client = BackpackClient::with_signer()?;
 
     let res = client.get_depth("SOL_USDC", None).await?;
 
